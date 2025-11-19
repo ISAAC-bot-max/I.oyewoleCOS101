b3bea11da74c4131d101531957a829e8a68752f3
Isaac Oyewole <isaac.oyewole@pau.edu.ng>
2025-11-19 16:50:45 +0100

    use of get(index)

--- week-8/practice_3/src/main.rs
+++ week-8/practice_3/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+//Method to print the get value 
+fn value(n:Option<&char>){
+
+    println!("Element of vector {:?}",n);
+
+}
+fn main(){
+
+    let rust = vec!['R','U','S','T','A','C','I','A','N'];
+    let mut input1 = String::new();
+    println!("\nEnter an index value between (0-8)");
+    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
+
+
+    //index is the no negative value which is smaller than the size of the vector 
+    let index:usize = input1.trim().parse().expect("Invalid input");
+
+
+    //getting value at given index value 
+    let ch: Option<&char> = rust.get(index);
+    value(ch);
+
 }
