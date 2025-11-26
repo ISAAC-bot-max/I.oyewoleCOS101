1733dd8d96bc72064ce7617401a20dc6f50fa6f5
Isaac Oyewole <isaac.oyewole@pau.edu.ng>
2025-11-26 15:45:34 +0100

    Open a File

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::fs::File;
+
+fn main(){
+    let mut file = File::create("Welcome_message.txt").unwrap();
+    let mut file = File::open("Welcome_message.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    print!("{}",contents);
 }
