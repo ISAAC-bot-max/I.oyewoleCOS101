a0705fcbe0823d4ff3d73d7ee6823bb2b0d742b6
Isaac Oyewole <isaac.oyewole@pau.edu.ng>
2025-11-26 16:33:47 +0100

    input & Save

--- week-9/practice_4/src/main.rs
+++ week-9/practice_4/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::OpenOptions; 
 use std::io::Write;
 use std::fs::File;
+use std::io;
 
 
 fn main(){
@@ -9,5 +10,12 @@ fn main(){
     let mut file = OpenOptions::new().append(true).open("data.txt").expect("Cannot open file");
     file.write_all("\nHello Class".as_bytes()).unwrap();
     file.write_all("\nThis is the appendage to the document".as_bytes()).unwrap();
+    file.write_all("\nMr Chukwudi is my Computer Science\n".as_bytes());
+
+    println!("Enter anything:__");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input1 = input.trim();
+    file.write_all(input1.as_bytes());
     println!("File append success");
 }
