48b27ff079ccaeb8aaf1985285610ea4c0514672
Isaac Oyewole <isaac.oyewole@pau.edu.ng>
2025-11-26 16:13:39 +0100

    practice_4

--- week-9/practice_4/src/main.rs
+++ week-9/practice_4/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::OpenOptions; 
+use std::io::Write;
+use std::fs::File;
+
+
+fn main(){
+
+    let mut file = File::create("data.txt").unwrap();
+    let mut file = OpenOptions::new().append(true).open("data.txt").expect("Cannot open file");
+    file.write_all("\nHello Class".as_bytes()).unwrap();
+    file.write_all("\nThis is the appendage to the document".as_bytes()).unwrap();
+    println!("File append success");
 }
