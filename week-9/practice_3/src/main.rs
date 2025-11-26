b676c7d5ac5760f513842e40f3b4b4516811bca7
Isaac Oyewole <isaac.oyewole@pau.edu.ng>
2025-11-26 15:50:55 +0100

    Removed a file (Created)

--- week-9/practice_3/src/main.rs
+++ week-9/practice_3/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::fs::File;
+
+fn main(){
+    let  file = File::create("data.txt").unwrap();
+    fs::remove_file("data.txt").expect("could not remove file");
+    println!("File is removed");
 }
